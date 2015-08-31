03a603ed4883bd6373125be1336bfedd0e978d89
rofrischmann <robin@rofrischmann.de>
2015-08-31 23:07:58 +0200

    #6 added propTypes for Box and abstracted flex properties for non flex developers
    this closes #6

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, {Component, PropTypes} from 'react';
 import Prefixer from 'inline-style-prefixer';
 import assign from 'assign-styles';
 
-let flexProps = [
-  'justifyContent', 'alignItems', 'alignContent', 'alignSelf', 'flexGrow', 'flexShrink', 'flexBasis', 'order'
-];
+let flexProps = {
+  grow: 'flexGrow',
+  shrink: 'flexShrink',
+  flow: 'flexFlow',
+  width: 'width',
+  height: 'height'
+}
 
-export default class Box extends React.Component {
+let alignVals = {
+  left: 'flex-start',
+  right: 'flex-end',
+  top: 'flex-start',
+  bottom: 'flex-end'
+}
+
+let alignProps = {
+  row: {
+    align: 'justifyContent',
+    verticalAlign: 'alignItems'
+  },
+  column: {
+    align: 'alignItems',
+    verticalAlign: 'justifyContent'
+  }
+}
+
+export default class Box extends Component {
   constructor() {
     super(...arguments);
   }
 
   render() {
     let props = this.props;
+    let orientation = props.column ? 'column' : 'row';
     let styles = {
       display : (props.inline ? 'inline-flex' : 'flex'),
       flex: (props.flex ? props.flex : '0 1 auto'),
-      flexDirection : (props.column ? 'column' : 'row') + (props.reverse ? '-reverse' : '')
+      flexDirection : orientation + (props.reverse ? '-reverse' : '')
     };
     if (props.wrap) {
       styles.flexWrap = 'wrap' + (props.wrap == 'reverse' ? '-reverse' : '');
@@ -34,11 +57,26 @@ export default class Box extends React.Component {
       styles.height = '100%';
     }
     
-    flexProps.forEach(property => {
-      if (props.hasOwnProperty(property)){
-        styles[property] = props[property];
+    let alignment = alignProps[orientation];
+    props.order && (styles.order = props.order);
+    
+    let prop;
+    for (prop in flexProps){
+      if (props[prop]){
+        styles[flexProps[prop]] = props[prop];
       }
-    });
+    }
+    
+    let alignProp;
+    for (alignProp in alignment){
+      if (props[alignProp]){
+        let value = props[alignProp];
+        if (alignVals[value]){
+          value = alignVals[value];
+        }
+        styles[alignment[alignProp]] = value;
+      }
+    }
     
     Prefixer.process(styles);
     
@@ -48,4 +86,30 @@ export default class Box extends React.Component {
     
     return <div {...this.props} style={styles}>{props.children}</div>;
   }
+}
+
+let CSSValues = [PropTypes.string, PropTypes.number];
+let alignValues = ['center', 'top', 'right', 'bottom', 'left', 'space-around', 'space-between', 'stretch', 'baseline'];
+
+Box.propTypes = {
+  flex: PropTypes.oneOfType(CSSValues),
+  grow : PropTypes.number,
+  shrink : PropTypes.number, 
+  width: PropTypes.oneOfType(CSSValues),
+  height: PropTypes.oneOfType(CSSValues),
+  flow: PropTypes.string,
+  order: PropTypes.number,
+  
+  inline: PropTypes.bool,
+  column: PropTypes.bool,
+  reverse: PropTypes.bool,
+  wrap : PropTypes.oneOf([PropTypes.bool, 'reverse']),
+  
+  align : PropTypes.oneOf(alignValues),
+  verticalAlign: PropTypes.oneOf(alignValues),
+  itemAlign : PropTypes.oneOf(alignValues),
+  lineAlign : PropTypes.oneOf(alignValues),
+
+  center:PropTypes.bool,
+  fit: PropTypes.bool
 }
