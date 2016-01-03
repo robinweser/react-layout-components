e274710424abeba43dd111a9be7bfa7630ff80b7
rofrischmann <robin@rofrischmann.de>
2016-01-03 01:57:59 +0100

    updated babel

--- modules/index.js
+++ modules/index.js
@@ -2,19 +2,17 @@ import Box from './components/Box'
 import Page from './components/Page'
 import Container from './components/Container'
 import ScrollView from './components/ScrollView'
-
 // shortcuts
 import Flex from './components/shortcut/Flex'
 import Center from './components/shortcut/Center'
 import VBox from './components/shortcut/VBox'
 
+export default Box
+
 export {
-	Box as default,
-	Box,
 	Page,
 	Container,
 	ScrollView,
-
 	Flex,
 	Center,
 	VBox
