7d106e0a1c7dd8548d7bfa42a88af6c0500ed082
David <david.bismut@gmail.com>
2016-01-28 11:39:56 +0100

    ...props -> ...this.props

--- modules/components/shortcut/Flex.jsx
+++ modules/components/shortcut/Flex.jsx
@@ -3,6 +3,6 @@ import Box from '../Box'
 
 export default class Flex extends Component {
   render() {
-    return <Box {...props} flex='1 0 auto' />
+    return <Box {...this.props} flex='1 0 auto' />
   }
 }
